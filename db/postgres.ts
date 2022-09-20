bc70aee49f80bbcf1f59712f6f441abaf0a82895
Brian Pham <phambri97@gmail.com>
2022-09-20 10:47:50 -0700

    Testing for deployment

--- db/postgres.ts
+++ db/postgres.ts
@@ -6,22 +6,7 @@ export const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  host: process.env.HOST
+  host: process.env.HOST,
+  port: 5432
 });
 
-// async function runDB () {
-//   console.log('starting up')
-
-//   const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-//   });
-//   try {
-//     await pool.connect();
-//     console.log('successfully connected')
-//   } catch (err) {
-//     console.log('MADE IT TO ERROR')
-//     console.error(err)
-//   }
-// }
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,23 @@
 import express, {Express} from 'express';
 import dotenv from 'dotenv';
 import { router } from './routes';
+import {pool} from '../db/postgres';
+
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use('/reviews', router);
+
+pool.connect()
+
+pool.query('SELECT * FROM reviews WHERE id = 1 limit 1', (err, res) => {
+  if (err) {console.log('psql db connection err')}
+  else {console.log('psql db connected')}
+})
+
+module.exports = pool;
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
