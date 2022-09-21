600aac8e84a025fd8d15b28a196681e61c112ca9
Brian Pham <phambri97@gmail.com>
2022-09-21 12:55:47 -0700

    Add morgan

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, {Express} from 'express';
 import dotenv from 'dotenv';
 import { router } from './routes';
 import {pool} from '../db/postgres';
+const morgan = require('morgan');
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ app.use(express.json());
 
 app.get('/loaderio-1d1ec1fdf04a9102c236cdde81fcfa85',
   (req, res) => res.send('loaderio-1d1ec1fdf04a9102c236cdde81fcfa85'))
-
+app.use(morgan('dev'));
 app.use('/reviews', router);
 
 pool.connect()
